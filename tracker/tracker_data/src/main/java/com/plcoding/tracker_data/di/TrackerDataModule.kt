5f593a15fd3acab675e27880a87cf0f6938a0332
Yann Chavret <yann.chavret@gmail.com>
2024-09-10 15:54:58 +0200

    TrackerRepository & Mappers

--- tracker/tracker_data/src/main/java/com/plcoding/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/plcoding/tracker_data/di/TrackerDataModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.room.Room
 import com.plcoding.tracker_data.local.TrackerDatabase
 import com.plcoding.tracker_data.remote.OpenFoodApi
+import com.plcoding.tracker_data.repository.TrackerRepositoryImpl
+import com.plcoding.tracker_domain.repository.TrackerRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -51,4 +53,17 @@ object TrackerDataModule {
             "tracker_db"
         ).build()
     }
+
+    @Provides
+    @Singleton
+    fun provideTrackerRepository(
+        api: OpenFoodApi,
+        db: TrackerDatabase
+    ): TrackerRepository {
+        return TrackerRepositoryImpl(
+            dao = db.dao,
+            api = api
+        )
+    }
+
 }
